8da44b12b3f612f95d8514281cc55225db301027
restudo <restudooctavriandi@gmail.com>
2022-11-18 14:15:29 +0700

    finish the backend

--- Assets/Scripts/!!NewInventoryTest/InventorySlot.cs
+++ Assets/Scripts/!!NewInventoryTest/InventorySlot.cs
@@ -8,7 +8,7 @@ public class InventorySlot
     [SerializeField] private InventoryItemData itemData;
     [SerializeField] private int stackSize;
 
-    public InventoryItemData data => itemData;
+    public InventoryItemData ItemData => itemData;
     public int stack => stackSize;
 
     public InventorySlot(InventoryItemData source, int amount)
@@ -26,6 +26,12 @@ public class InventorySlot
     {
         itemData = null;
         stackSize = -1;
+    }
+
+    public void UpdateInventorySlot(InventoryItemData data, int amount)
+    {
+        itemData = data;
+        stackSize = amount;
     }
 
     public bool RoomLeftInStack(int amountToAdd, out int amountRemaining)
--- Assets/Scripts/!!NewInventoryTest/InventorySystem.cs
+++ Assets/Scripts/!!NewInventoryTest/InventorySystem.cs
@@ -25,8 +25,42 @@ public class InventorySystem
 
     public bool AddToInventory(InventoryItemData itemToAdd, int amountToAdd)
     {
-        inventorySlots[0] = new InventorySlot(itemToAdd, amountToAdd);
-        return true;
+        // check whether item exist in inventory
+        if (ContainsItem(itemToAdd, out List<InventorySlot> invSlot))
+        {
+            foreach (var slot in invSlot)
+            {
+                if (slot.RoomLeftInStack(amountToAdd))
+                {
+                    slot.AddToStack(amountToAdd);
+                    OnInventorySlotChanged?.Invoke(slot);
+                    return true;
+                }
+            }
+        }
+
+        // get the first available slot
+        if (HasFreeSlot(out InventorySlot freeSlot))
+        {
+            freeSlot.UpdateInventorySlot(itemToAdd, amountToAdd);
+            OnInventorySlotChanged?.Invoke(freeSlot);
+            return true;
+        }
+
+        return false;
+
     }
 
+    public bool ContainsItem(InventoryItemData itemToAdd, out List<InventorySlot> invSlot)
+    {
+        invSlot = inventorySlots.Where(i => i.ItemData == itemToAdd).ToList();
+
+        return invSlot == null ? false : true;
+    }
+
+    public bool HasFreeSlot(out InventorySlot freeSlot)
+    {
+        freeSlot = InventorySlots.FirstOrDefault(i => i.ItemData == null);
+        return freeSlot == null ? false : true;
+    }
 }
